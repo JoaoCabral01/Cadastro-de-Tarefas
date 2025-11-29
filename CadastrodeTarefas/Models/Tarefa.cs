fbefcfdf587a2bc24729759aca61856d6880dfb8
joao <joaoh10cabral@gmail.com>
2025-11-29 11:17:06 -0300

    Criando a estrutura do CRUD

--- CadastrodeTarefas/Models/Tarefa.cs
+++ CadastrodeTarefas/Models/Tarefa.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CadastrodeTarefas.Models
+﻿namespace TodoApp.Models
 {
-    internal class Tarefa
+    public class Tarefa
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
     }
 }
