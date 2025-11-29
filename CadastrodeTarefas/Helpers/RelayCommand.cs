873ee5cc533a1ab466e879c20070c3d94369493d
joao <joaoh10cabral@gmail.com>
2025-11-29 11:37:06 -0300

    Adicionei o RelayCommand para poder permitir o MVVM no xaml

---
+++ CadastrodeTarefas/Helpers/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TodoApp.Helpers
+{
+   public class RelayComand : ICommand
+    {
+        private readonly Action<object> _execute;
+        
+        public RelayComand (Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+              
+}
+
