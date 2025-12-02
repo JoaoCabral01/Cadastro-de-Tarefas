d1318c9f2eefe942cebbd6139fac980175fe9d9d
joao <joaoh10cabral@gmail.com>
2025-12-02 11:56:34 -0300

    Zoom na imagem e "ultima" modificação do Cadastro de Tarefas

--- CadastrodeTarefas/Helpers/RelayCommand.cs
+++ CadastrodeTarefas/Helpers/RelayCommand.cs
@@ -20,5 +20,6 @@ namespace CadastroDeTarefas.Helpers
         {
             _execute(parameter);
         }
+
     }
 }
--- CadastrodeTarefas/MainWindow.xaml.cs
+++ CadastrodeTarefas/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-using CadastroDeTarefas.Models;
-using CadastroDeTarefas.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using CadastroDeTarefas.ViewModels;
 
 namespace CadastroDeTarefas
 {
@@ -16,6 +16,9 @@ namespace CadastroDeTarefas
 
             if (this.Content is FrameworkElement root)
                 root.DataContext = ViewModel;
+
+            ImagemAlbon.PointerEntered += ImagemAlbon_PointerEntered;
+            ImagemAlbon.PointerExited += ImagemAlbon_PointerExited;
         }
 
         private void Adicionar_Click(object sender, RoutedEventArgs e)
@@ -25,12 +28,22 @@ namespace CadastroDeTarefas
 
         private void Excluir_Click(object sender, RoutedEventArgs e)
         {
-            if (sender is Button btn && btn.DataContext is Tarefa tarefa)
+            if (sender is Button btn && btn.DataContext is Models.Tarefa tarefa)
             {
                 ViewModel.Tarefas.Remove(tarefa);
             }
         }
 
-    }
+        private void ImagemAlbon_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            ScaleImagem.ScaleX = 1.2;
+            ScaleImagem.ScaleY = 1.2;
+        }
 
+        private void ImagemAlbon_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ScaleImagem.ScaleX = 1;
+            ScaleImagem.ScaleY = 1;
+        }
+    }
 }
